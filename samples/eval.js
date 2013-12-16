ba1592ddd7cf48d91ef92991e296f49444ec4374
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-12-15 16:20:45 -0800

    extend the checker, add some more tests

--- samples/eval.js
+++ samples/eval.js
@@ -9,3 +9,18 @@ var m = function(){ return eval; }();
 
 var j = function(){ return eval(); }();
 
+a = eval;
+
+a = eval();
+
+if(eval()) { }
+
+a = 1 ? eval : null;
+
+window
+
+setTimeout();
+
+window.setTimeout(function(){alert('Hello')}, 1000);
+
+
